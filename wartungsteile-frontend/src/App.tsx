e38d4a9bcef809fef503418fd231e3d031431003
KadirBerbergil <kadir@berbergil.de>
2025-05-25 00:25:28 +0200

    gg

--- wartungsteile-frontend/src/App.tsx
+++ wartungsteile-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-// src/App.tsx - Mit aktiviertem MachineMaintenanceWorkflow
+// src/App.tsx - Mit PDF Upload Route hinzugefügt
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Dashboard from './pages/dashboard/Dashboard'
@@ -6,6 +6,7 @@ import MachineList from './pages/machines/MachineList'
 import MachineDetail from './pages/machines/MachineDetail'
 import MachineMaintenancePartsList from './pages/machines/MachineMaintenancePartsList'
 import MachineMaintenanceWorkflow from './pages/machines/MachineMaintenanceWorkflow'
+import PdfUploadExtractor from './pages/machines/PdfUploadExtractor'
 import MaintenancePartsList from './pages/parts/MaintenancePartsList'
 import MaintenancePartDetail from './pages/parts/MaintenancePartDetail'
 import MaintenancePartEdit from './pages/parts/MaintenancePartEdit'
@@ -20,11 +21,12 @@ function App() {
         <Route path="machines/:id" element={<MachineDetail />} />
         <Route path="machines/:id/parts" element={<MachineMaintenancePartsList />} />
         <Route path="machines/:id/maintenance" element={<MachineMaintenanceWorkflow />} />
+        {/* ✅ NEUE ROUTE: PDF Upload für intelligente Maschinenerstellung */}
+        <Route path="machines/upload" element={<PdfUploadExtractor />} />
         <Route path="parts" element={<MaintenancePartsList />} />
         <Route path="parts/new" element={<MaintenancePartCreate />} />
         <Route path="parts/:id" element={<MaintenancePartDetail />} />
-        {/* Temporär auskommentiert bis Export-Problem behoben ist */}
-        {/* <Route path="parts/:id/edit" element={<MaintenancePartEdit />} /> */}
+        <Route path="parts/:id/edit" element={<MaintenancePartEdit />} />
       </Route>
     </Routes>
   )
